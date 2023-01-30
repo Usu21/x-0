adef24f1c53df3c28e96545db653c87f610fd025
Usu21 <98323022+Usu21@users.noreply.github.com>
2023-01-30 18:08:30 +0500

    Add files via upload

---
+++ lesson 34 крестики-нолики/script.js
@@ -0,0 +1,67 @@
+let cells = document.querySelectorAll('#field td');
+let winner = document.querySelector('#winner');
+let close = document.querySelector('#close');
+let text = document.querySelector('#text');
+
+console.log(cells);
+
+let i = 0;
+function start(cells) {
+    for (let cell of cells) {
+        cell.addEventListener('click', function step() {
+            if (i % 2 == 0) {
+                this.textContent = 'X';
+            } else {
+                this.textContent = '0';
+            }
+
+            this.removeEventListener('click', step);
+            if (isWinner(cells) == true) {
+                winner.style.display = 'flex';
+                text.textContent = `Победитель ${this.textContent}`;
+            } else if (i == 8) {
+                winner.style.display = 'flex';
+                text.textContent = `Ничья`;
+            };
+            i++;
+        })
+        close.addEventListener('click', function close() {
+            winner.style.display = 'none';
+            cell.textContent = '';
+
+        })
+        i = 0
+        console.log(i)
+        this.removeEventListener('click', close)
+    }
+}
+
+start(cells);
+
+function isWinner(cells) {
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent == cells[comb[1]].textContent && cells[comb[1]].textContent ==
+            cells[comb[2]].textContent && cells[comb[0]].textContent != '') {
+            return true;
+        }
+    }
+    return false;
+}
+
+// let comb
+// comb = [0, 1, 2]
+// если (cells[0]) == (cells[1]) ==  cells[2]]
+// true
+
+// comb = [3, 4, 5]
